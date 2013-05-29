5557440ead4e660177e0b320ba51538972ac86fe
WP How <wphow@rojak.net>
2013-05-29 05:20:28 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampleapp::Application.config.secret_token = '51816589adc55af00a4ea052be5aa712a575279f8ee8da74b7d6cf9ab7923893c2d7347e0069fe30f419dd4af72946901339a8b5cf28496f8cfd1d9eb088d578'
